5fee8148065216c3447660e67bf979f6446b363f
Dima Pidlisniy <dima.pidlisniy@gmail.com>
2023-12-02 22:21:20 +0200

    final

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const refs = {
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
   infoForUser: document.querySelector('.info-for-user'),
-  loader: document.querySelector('.loader'),
   observerElement: document.querySelector('.observer-element'),
   btn: document.getElementsByName('btn-submit'),
 };
@@ -24,13 +23,12 @@ async function handleSubmit(event) {
   searchWord = event.currentTarget
     .querySelector('[name="searchQuery"]')
     .value.trim();
-  observer.observe(refs.observerElement);
-  console.log(refs.observerElement);
+
   currentHits = 0;
   page = 1;
-
+  refs.infoForUser.classList.add('is-hidden');
   if (searchWord === '') {
-    refs.loader.classList.add('is-hidden');
+    refs.loadMoreBtn.classList.add('is-hidden');
     refs.infoForUser.classList.add('is-hidden');
     refs.gallery.innerHTML = '';
     return Notiflix.Notify.info('Please, enter at least one letter!');
@@ -39,12 +37,11 @@ async function handleSubmit(event) {
   try {
     const searchObjects = await fetchResult(searchWord, page);
     const selectHits = searchObjects.hits.length;
+
     currentHits += selectHits;
     console.log(currentHits, searchObjects.totalHits);
 
     if (searchObjects.hits.length === 0) {
-      observer.unobserve(refs.observerElement);
-      refs.loader.classList.add('is-hidden');
       refs.infoForUser.classList.add('is-hidden');
       refs.gallery.innerHTML = '';
       return Notiflix.Notify.failure(
@@ -58,20 +55,17 @@ async function handleSubmit(event) {
     let simpleLightBox = new SimpleLightbox('.gallery a', {
       captions: false,
     });
+
+    refs.loadMoreBtn.classList.remove('is-hidden');
+    if (searchObjects.hits.length < 40) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+    }
     refs.btn[0].disabled = true;
     refs.btn[0].classList.add('btn-submit');
-    refs.loader.classList.remove('is-hidden');
-    refs.loader.classList.add('loader');
+
     refs.searchForm.addEventListener('input', inputChange);
 
-    if (currentHits < searchObjects.totalHits) {
-      observer.observe(refs.observerElement);
-    }
-
     if (currentHits === searchObjects.totalHits) {
-      scrollFunction();
-      observer.unobserve(refs.observerElement);
-      refs.loader.classList.add('is-hidden');
       refs.infoForUser.classList.remove('is-hidden');
     }
   } catch (error) {
@@ -97,36 +91,54 @@ async function handleScrollToBottom(entries) {
         const searchObjects = await fetchResult(searchWord, page);
         const selectHits = searchObjects.hits.length;
         currentHits += selectHits;
-        console.log(searchObjects.totalHits);
-        console.log(entry.isIntersecting);
+
         refs.infoForUser.classList.add('is-hidden');
         let hits = searchObjects.hits;
-        refs.gallery.innerHTML += Markup(hits);
+
         let simpleLightBox = new SimpleLightbox('.gallery a', {
           captions: false,
         });
 
         simpleLightBox.refresh();
-        pageScroll();
 
         if (searchObjects.totalHits === currentHits) {
           console.log(entry.isIntersecting);
-          refs.loader.disabled = true;
-          refs.loader.classList.remove('loader');
-          refs.loader.classList.add('is-hidden');
           refs.infoForUser.classList.remove('is-hidden');
-          observer.unobserve(refs.observerElement);
         }
       }
     } catch (error) {
       Notiflix.Notify.failure(
-        '444 Sorry, there was an error loading more images. Please try again.'
+        'Sorry, there was an error loading more images. Please try again.'
       );
     }
   }
 }
 
-observer.observe(refs.observerElement);
+refs.loadMoreBtn.addEventListener('click', handleClick);
+
+async function handleClick() {
+  page += 1;
+  refs.loadMoreBtn.classList.add('load-more');
+  const searchObjects = await fetchResult(searchWord, page);
+  const selectHits = searchObjects.hits.length;
+  currentHits += selectHits;
+  let hits = searchObjects.hits;
+  refs.gallery.innerHTML += Markup(hits);
+  let simpleLightBox = new SimpleLightbox('.gallery a', {
+    captions: false,
+  });
+  simpleLightBox.refresh();
+
+  if (searchObjects.totalHits === currentHits) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    refs.loadMoreBtn.disabled = true;
+    refs.loadMoreBtn.classList.remove('load-more');
+    refs.loadMoreBtn.classList.add('is-hidden');
+    refs.infoForUser.classList.remove('is-hidden');
+  }
+}
 
 function Markup(hits) {
   return hits
